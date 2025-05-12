ae18e27362134d712fa532d1457012b26fd791fe
Hanna Sophie Muriel Lima <1110366475@u.icesi.edu.co>
2025-05-12 08:22:09 -0500

    Login

--- js/login.js
+++ js/login.js
@@ -13,3 +13,35 @@
  * 8. Guarda el usuarioLogueado en el LocalStorage
  * 9. Redirecciona al index.html
  */
+
+const loginForm = document.getElementById('loginForm')
+
+function ingresarUsuario(e) {
+    e.preventDefault()
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || []
+    const existeUsuario = usuarios.find((usuario) => usuario.email === email)
+
+    if (!existeUsuario) {
+        alert ("El usuario no existe, por favor registralo!")
+        loginForm.reset()
+        return
+    }
+
+    if (existeUsuario.password !== password){
+        alert ("La contraseña es incorrecta!")
+        loginForm.reset()
+        return
+    }
+
+    const usuarioLogueado = {
+        nombreCompleto: existeUsuario.nombre +" "+ existeUsuario.apellido,
+        email: existeUsuario.email
+    }
+
+    localStorage.setItem('logueado', JSON.stringify(usuarioLogueado))
+    window.location.href = 'index.html'
+}
+
+loginForm.addEventListener('submit', ingresarUsuario)
